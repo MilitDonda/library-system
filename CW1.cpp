1f0d45dbcd9d58c32d5748ce13a163a150caf181
Milit Donda <militdonda3@gmail.com>
2024-01-15 10:12:47 +0000

    switch cases 2 and 3 filled along with added functionality for displaying books, issuing books and returning them

--- CW1.cpp
+++ CW1.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #include <chrono>
 #include <iomanip>
-
+#include <fstream>
+#include <sstream>
 
 class Book{
 public:
@@ -19,6 +20,22 @@ public:
         dueDate = std::chrono::system_clock::now(); 
     };
 
+    std::string getbookID(){
+        return std::to_string(bookID);
+    }
+
+    std::string getbookName(){
+        return bookName;
+    }
+
+    std::string getAuthorFirstName(){
+        return authorFirstName;
+    }
+
+    std::string getAuthorLastName(){
+        return authorLastName;
+    }
+
     void setDueDate(int days) {
         dueDate = std::chrono::system_clock::now() + std::chrono::hours(24 * days);
     }
@@ -28,9 +45,13 @@ public:
         return std::ctime(&dueTime);
     } 
 
-    int getbookID(){
-        return bookID;
+    void returnBook(){
+        //code to add set the flag to true in the csv file when the book is returned
     }
+
+    /*void borrowBook(Member borrower, int dueAfterDays){
+        //boorow book code here
+    }*/
 };
 
 class Person{
@@ -75,7 +96,11 @@ public:
         return std::to_string(memberId);
     }
     void setBooksBorrowed(Book){
+        //code to set books borrowed goes here
+    }
 
+    std::vector <Book> getBooksBorrowed(){
+        return booksLoaned;
     }
 
 };
@@ -96,9 +121,85 @@ public:
         salary = Salary;
     };
 
+    bool memberExists(int memberID, std::string Name, std::string Address, std::string Email){
+        for(Member member : members){
+            if(member.getMemberId() == std::to_string(memberID) ||
+                (member.getName() == Name && member.getAdrress() == Address && member.getEmail() == Email)){
+                    return true;
+                }
+       }
+       return false;
+    };
+
     void addMember(int memberID, std::string Name, std::string Address, std::string Email) {
-        Member newMember(memberID, Name, Address, Email);
-        members.push_back(newMember);
+        if (!memberExists(memberID, Name, Address, Email)) {
+            Member newMember(memberID, Name, Address, Email);
+            members.push_back(newMember);
+            std::cout << "New member added successfully.\n";
+        } else {
+            std::cout << "Member already exists.\n";
+        }
+    };
+
+    void displayLibraryBooks(){
+        std::fstream myFile;
+        myFile.open("library_books.csv", std::ios::in);
+        if(myFile.is_open()){
+            std::string line;
+            while(getline(myFile, line)){
+                std::cout << line << std::endl;
+                std::cout << std::endl;
+            }
+            myFile.close();
+            }
+    }
+
+    struct IssuedBooks{
+        int bookID;
+        int memberID;
+    };
+
+    std::vector<IssuedBooks> issuedBooks;
+
+    bool isBookIssued(int bookId){
+        for (IssuedBooks issuedBook : issuedBooks){
+            if(issuedBook.bookID == bookId){
+                return true;
+            }
+        }
+        return false;
+    };
+
+    void issueBook(int memberId, int bookId){
+        if (isBookIssued(bookId)) {
+            std::cout << "Book is already issued." << std::endl;
+        return;
+    }
+
+    IssuedBooks newIssuedBook = {bookId, memberId};
+    issuedBooks.push_back(newIssuedBook);
+    std::cout << "Book issued successfully." << std::endl;
+}
+
+    void returnBook(int memberId, int bookId){
+        for (int i = 0; i < issuedBooks.size(); ){
+            if(issuedBooks[i].memberID == memberId && issuedBooks[i].bookID == bookId){
+            issuedBooks.erase(issuedBooks.begin() + i);
+            } else {
+                ++i;
+            }
+
+        }
+    }
+
+    void displayBorrowedBooks(int memberID){
+        for (int i = 0; i < issuedBooks.size(); i++){
+            std::cout<< issuedBooks[i].bookID << std::endl;
+        }
+    }
+
+    void calcFine(int memberID){
+        //code to calculate fine goes here
     }
 
     void setStaffID(int staffID){
@@ -123,7 +224,73 @@ int main(){
 using std::cout;
 using std::endl;
 
-    cout << "Welcome to the library" << endl;
-    cout << "Please select an option from below to get started" << endl;
+Librarian librarian(1, "Milit", "21 blackwell place", "militdonda3@gmail.com", 100);
 
+    int userChoice;
+    int i = 0;
+    while(i < 2){
+        int memberID;
+        std::string Name;
+        std::string Address;
+        std::string Email;
+        int bookID;
+
+        cout << "---------------------------------------------------" << endl;
+        cout << "Welcome to the library" << endl;
+        cout << "Please select an option from below to get started" << endl;
+        cout << "[1] Add/Register members" << endl;
+        cout << "[2] issue a book (requires you to be registered)" << endl;
+        cout << "[3] return a book" << endl;
+        cout << "[4] Display all your borrowed books" << endl;
+        cout << "[5] calculate fine (if the due date was missed)" << endl;
+        cout << "[6] Exit" <<endl;
+        cout << "---------------------------------------------------" << endl;
+
+        std::cin >> userChoice;  
+        switch(userChoice){
+            case 1:
+                cout << "Enter memberID: " << endl;
+                std::cin >> memberID;
+                cout << "Enter your name: " << endl;
+                std::cin >> Name;
+                cout << "Enter your address: " << endl;
+                std::cin >> Address;
+                cout << "Enter Email address: " << endl;
+                std::cin >> Email;
+                librarian.addMember(memberID, Name, Address, Email);
+            break;
+
+            case 2:
+                cout<<"Please have a look through our collection of books and note the BookID of the book you would like to issue"<<endl;
+                cout <<endl;
+                librarian.displayLibraryBooks();
+                cout <<"enter memberID: " << endl;
+                std::cin >> memberID;
+                cout <<"enter bookID: " << endl;
+                std::cin >> bookID;
+                librarian.issueBook(memberID, bookID);
+            break;
+
+            case 3:
+                cout << endl;
+                cout<< "Enter bookID of the book you would like to return: "<<endl;
+                std::cin >> bookID;
+                cout << "Enter your memberId: " << endl;
+                std::cin >> memberID;
+                librarian.returnBook(memberID, bookID);
+            break;
+            case 4:
+                cout << "Enter your memberID: " << endl;
+                std::cin >> memberID;
+                librarian.displayBorrowedBooks(memberID);
+
+            break;
+            case 5:
+            break;
+            case 6:
+            i = 2;
+            break;
+        }   
+
+    }
 }
